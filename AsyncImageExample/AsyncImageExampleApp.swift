1bcab5e7a362ff18270aeaf1e7751d96cdb0c6e9
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-11-22 15:44:24 +0300

    Initial Commit

---
+++ AsyncImageExample/AsyncImageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageExampleApp.swift
+//  AsyncImageExample
+//
+//  Created by Fatih Durmaz on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
